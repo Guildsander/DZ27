bc9563b229a7318dbd5b65c80a677871128b6f50
carac <caracin9000@gmail.com>
2023-08-20 10:47:07 +0300

    DZ26

--- venv/DZ26.py
+++ venv/DZ26.py
@@ -1,7 +1,16 @@
 def first_word(text):
     """ Пошук першого слова """
-    pass
+    i = 0
+    while i < len(text) and not text[i].isalpha():
+        i += 1
 
+    j = i
+    while j < len(text) and (text[j].isalpha() or text[j] == "'"):
+        j += 1
+
+    return text[i:j]
+
+# Тести
 assert first_word("Hello world") == "Hello", 'Test1'
 assert first_word("greetings, friends") == "greetings", 'Test2'
 assert first_word("don't touch it") == "don't", 'Test3'
@@ -9,3 +18,4 @@ assert first_word(".., and so on ...") == "and", 'Test4'
 assert first_word("hi") == "hi", 'Test5'
 assert first_word("Hello.World") == "Hello", 'Test6'
 print('OK')
+
